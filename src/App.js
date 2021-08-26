f0c51a6066dd15aa5b7ec0c2600be773a294eb74
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-08-26 13:59:31 -0200

    5ta funcionalidad implementada: Saludar tomando en cuenta la hora

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
 const genero = document.querySelector("#genero-input");
 
+var fechaHoraActual = new Date();
+var horaActual = fechaHoraActual.toLocaleTimeString('it-IT');
+
+function getSaludoSegunTiempo(hora){
+    saludoSegunTiempo = "buenos días ";
+    if (hora > '12:00:00' && hora < '18:30:00'){
+        saludoSegunTiempo = "buenas tardes ";
+    }else if (hora > '18:30:00' && hora < '23:59:59'){
+        saludoSegunTiempo = "buenos noches ";
+    }
+    return saludoSegunTiempo;
+}
+
+
 const form = document.querySelector("#saludador-form");
 
 form.addEventListener("submit", event =>{
-    mensaje = "Hola ";
+    mensaje = "Hola, " + getSaludoSegunTiempo(horaActual);
     if(edad.value > 30){
         if(genero.value == "femenino"){
             mensaje = mensaje + "señora ";  
         }else{  
-            mensaje = mensaje + "señor ";  
+            mensaje = mensaje + " señor ";  
         }
     } else if (edad.value > 16) {
         if(genero.value == "femenino"){
