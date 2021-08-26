d4dc500b3a502cbd2aa4171da754454afcf83bca
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-08-26 14:57:46 -0200

    6ta funcionalidad implementada: Saludar segun el idioma

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
 const genero = document.querySelector("#genero-input");
+const idioma = document.querySelector("#idioma-input");
+
+const form = document.querySelector("#saludador-form");
 
 var fechaHoraActual = new Date();
 var horaActual = fechaHoraActual.toLocaleTimeString('it-IT');
 
-function getSaludoSegunTiempo(hora){
-    saludoSegunTiempo = "buenos días ";
+function getSaludoSegunTiempo(hora, mensajeMañana, mensajeTarde, mensajeNoche){
+    saludoSegunTiempo = mensajeMañana;
     if (hora > '12:00:00' && hora < '18:30:00'){
-        saludoSegunTiempo = "buenas tardes ";
+        saludoSegunTiempo = mensajeTarde;
     }else if (hora > '18:30:00' && hora < '23:59:59'){
-        saludoSegunTiempo = "buenos noches ";
+        saludoSegunTiempo = mensajeNoche;
     }
     return saludoSegunTiempo;
 }
 
-
-const form = document.querySelector("#saludador-form");
-
-form.addEventListener("submit", event =>{
-    mensaje = "Hola, " + getSaludoSegunTiempo(horaActual);
+function getTituloPersonalSegunEdad(edad, mayor30F, mayor30M, mayor16F, mayor16M){
+    var tituloPersonal = "";
     if(edad.value > 30){
         if(genero.value == "femenino"){
-            mensaje = mensaje + "señora ";  
+            tituloPersonal = tituloPersonal + mayor30F;  
         }else{  
-            mensaje = mensaje + " señor ";  
+            tituloPersonal = tituloPersonal + mayor30M;  
         }
     } else if (edad.value > 16) {
         if(genero.value == "femenino"){
-            mensaje = mensaje + "señorita ";  
+            tituloPersonal = tituloPersonal + mayor16F;  
         }else{  
-            mensaje = mensaje + "joven ";  
+            tituloPersonal = tituloPersonal + mayor16M;  
         }
     }
+    return tituloPersonal;
+}
+
+form.addEventListener("submit", event =>{
+    if(idioma.value == "español"){
+        console.log("in");
+        mensaje = "Hola, " + getSaludoSegunTiempo(horaActual,"buenos días ","buenas tardes ","buenos noches ");
+        mensaje = mensaje + getTituloPersonalSegunEdad(edad,"señora ","señor ","señorita ", "joven ");
+    }
+    else{
+        mensaje = "Hello, " + getSaludoSegunTiempo(horaActual,"good morning ","good afternoon ","good night ");
+        mensaje = mensaje + getTituloPersonalSegunEdad(edad,"Mrs. ","Mr. ","Miss ", "Mr. ");
+    }
+    
     alert(mensaje + nombre.value); 
 })
