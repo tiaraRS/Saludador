4d766a7eede806fe543e437bbc772c735a9ed013
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-08-26 11:15:56 -0200

    3ra funcionalidad implementada: Saludar tomando en cuenta la edad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 const nombre = document.querySelector("#nombre-input");
+const edad = document.querySelector("#edad-input");
 
 const form = document.querySelector("#saludador-form");
 
 form.addEventListener("submit", event =>{
-    alert("Hola " + nombre.value); 
+    mensaje = "Hola ";
+    if(edad.value > 30){
+        mensaje = mensaje + "señor(a) ";
+    } else if (edad.value > 16) {
+        mensaje = mensaje + "joven/señorita "
+    }
+    alert(mensaje + nombre.value); 
+
 })
